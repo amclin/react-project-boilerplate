f8db8dffbe938538c4fb951b2942491a66ae1f1c
Anthony McLin <anthony.mclin@rosetta.com>
2019-09-30 18:04:23 -0500

    feat(tests): use jest for unit tests

---
+++ pages/index.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './index';
+
+describe('Pages:Homepage', () => {
+  it('renders the homepage', () => {
+    const component = renderer.create(<App />)
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  })
+})
