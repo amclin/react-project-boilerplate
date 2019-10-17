a5064a9f71bbcc7c5f2827f9e34f9e194861c936
Anthony McLin <anthony.mclin@rosetta.com>
2019-10-17 13:45:08 -0500

    fix: incorrect import paths in generated unit tests

--- templates/default/src/__tests__/pages/example/second-page/index.test.jsx
+++ templates/default/src/__tests__/pages/example/second-page/index.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import App from '../../../../../pages/example/second-page'
+import App from '../../../../pages/example/second-page'
 
 describe('Pages:SecondPage', () => {
   it('renders a second example page', () => {
