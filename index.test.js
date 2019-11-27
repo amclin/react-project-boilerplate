36bffdf1c146ca5d3c976f41b148f4c26577fd40
Kevin Danikowski <kevindanikowski@Kevins-MacBook-Pro.local>
2019-11-27 09:05:44 -0600

    fix(test command): tmp dir now use random int to not conflict

--- index.test.js
+++ index.test.js
@@ -4,97 +4,92 @@ const { execSync } = require('child_process')
 const rimraf = require('rimraf')
 const { error } = require('./helpers/logger')
 
+const tmpDir = `tmp-${Math.floor(Math.random() * 1000000)}`
+
 // TODO: Jest can't process coverage of spaned processes
 // May need to wrap NYC to get the coverage of all the
 // code executed here
 // https://github.com/amclin/react-project-boilerplate/issues/28
 
 describe('Integration Test', () => {
-describe('Generated App', () => {
-  beforeAll( async () => {
-    // Run the generator expecting successful STDOUT
-    try {
-      await execSync('node ./index.js --use-npm --no-git --with-ssr -- tmp', { stdio: 'inherit' })
-    } catch (e) {
-      error('Failed to complete generation process.', e)
-      expect(true).toEqual(false) // Force test failure
-    }
-  })
-  afterAll(() => {
-    // Cleanup the temp
-    rimraf.sync('./tmp', {}, () => {
-      error('No tmp directory to remove.')
-      expect(true).toEqual(false) // Force test failure
-    })
-  })
-
-  it('can build', async () => {
-    // Generated app should be buildable
-    try {
-      await execSync('(cd tmp ; npm run build)', { stdio: 'inherit' })
-    } catch (e) {
-      error('Generated app failed to build with `npm run build`', e)
-      expect(true).toEqual(false) // Force test failure
-    }
-  })
-
-  it('can export static html', async () => {
-    // Generated app should be exportable
-    try {
-      await execSync('(cd tmp ; npm run export)', { stdio: 'inherit' })
-    } catch (e) {
-      error('Generated app failed to export with `npm run export`', e)
-      expect(true).toEqual(false) // Force test failure
-    }
-  })
-
-  describe('New component wizard(plop)', () => {
-    const componentTypes = {
-      atom: {
-        files: [
-          'index.js',
-          'MockComponent.jsx',
-          'MockComponent.test.jsx'
-        ]
-      },
-      molecule: {
-        files: [
-          'index.js',
-          'MockComponent.jsx',
-          'MockComponent.test.jsx'
-        ]
-      },
-      organism: {
-        files: [
-          'index.js',
-          'MockComponent.jsx',
-          'MockComponent.test.jsx'
-        ]
+  describe('Generated App', () => {
+    beforeAll(async () => {
+      // Run the generator expecting successful STDOUT
+      try {
+        await execSync(
+          `node ./index.js --use-npm --no-git --with-ssr -- ${tmpDir}`,
+          { stdio: 'inherit' }
+        )
+      } catch (e) {
+        error('Failed to complete generation process.', e)
+        expect(true).toEqual(false) // Force test failure
       }
-    }
-
-    // Loop through each type of component and test the files are created
-    Object.entries(componentTypes).forEach(([componentType, data]) => {
-      it(`can generate new ${componentType}s through the wizard`, async() => {
-        const path = `tmp/src/components/${componentType}s/MockComponent`
-        const cmd = `(cd tmp; npm run generate -- component ${componentType} "mock component" "short description")`
-
-        try {
-          await execSync(cmd)
-        } catch (e) {
-          error(`Failed to run plop for a ${componentType}`, e)
-          expect(true).toEqual(false) // Force test failure
-        }
-
-        // Check that all expected files are generated
-        data.files.forEach((file) => {
-          expect(fs.readFileSync(`${path}/${file}`, 'utf-8')).toMatchSnapshot()
-        })
+    })
+    afterAll(() => {
+      // Cleanup the temp
+      rimraf.sync(`./${tmpDir}`, {}, () => {
+        error(`No tmp directory (${tmpDir}) to remove.`)
+        expect(true).toEqual(false) // Force test failure
       })
     })
 
-    it.skip('can generate new pages through the wizard', async () => {})
-    it.skip('can generate new apis through the wizard', async () => {})
+    it('can build', async () => {
+      // Generated app should be buildable
+      try {
+        await execSync(`(cd ${tmpDir} ; npm run build)`, { stdio: 'inherit' })
+      } catch (e) {
+        error('Generated app failed to build with `npm run build`', e)
+        expect(true).toEqual(false) // Force test failure
+      }
+    })
+
+    it('can export static html', async () => {
+      // Generated app should be exportable
+      try {
+        await execSync(`(cd ${tmpDir} ; npm run export)`, { stdio: 'inherit' })
+      } catch (e) {
+        error('Generated app failed to export with `npm run export`', e)
+        expect(true).toEqual(false) // Force test failure
+      }
+    })
+
+    describe('New component wizard(plop)', () => {
+      const componentTypes = {
+        atom: {
+          files: ['index.js', 'MockComponent.jsx', 'MockComponent.test.jsx']
+        },
+        molecule: {
+          files: ['index.js', 'MockComponent.jsx', 'MockComponent.test.jsx']
+        },
+        organism: {
+          files: ['index.js', 'MockComponent.jsx', 'MockComponent.test.jsx']
+        }
+      }
+
+      // Loop through each type of component and test the files are created
+      Object.entries(componentTypes).forEach(([componentType, data]) => {
+        it(`can generate new ${componentType}s through the wizard`, async () => {
+          const path = `${tmpDir}/src/components/${componentType}s/MockComponent`
+          const cmd = `(cd ${tmpDir}; npm run generate -- component ${componentType} "mock component" "short description")`
+
+          try {
+            await execSync(cmd)
+          } catch (e) {
+            error(`Failed to run plop for a ${componentType}`, e)
+            expect(true).toEqual(false) // Force test failure
+          }
+
+          // Check that all expected files are generated
+          data.files.forEach(file => {
+            expect(
+              fs.readFileSync(`${path}/${file}`, 'utf-8')
+            ).toMatchSnapshot()
+          })
+        })
+      })
+
+      it.skip('can generate new pages through the wizard', async () => {})
+      it.skip('can generate new apis through the wizard', async () => {})
+    })
   })
-})
 })
