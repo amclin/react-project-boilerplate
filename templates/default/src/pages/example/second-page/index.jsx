6d013e38177cb487c8c7dfb97d23177d48b02148
Kevin Danikowski <kevindanikowski@Kevins-MacBook-Pro.local>
2019-11-27 10:26:04 -0600

    feat(_document.js): refactored globalhead to now exist in _document.js

--- templates/default/src/pages/example/second-page/index.jsx
+++ templates/default/src/pages/example/second-page/index.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
-import GlobalHead from '../../../components/organisms/GlobalHead'
+import PageTitle from '../../../components/organisms/PageTitle'
 
 const SecondPage = () => {
   return (
     <>
-      <GlobalHead title="Second Page - %%APPNAME%%" />
+      <PageTitle title="Second Page - %%APPNAME%%" />
       <main>
-        <p>
-          This is another page at a different URL.
-        </p>
+        <p>This is another page at a different URL.</p>
         <p>
           Return to the
-
           <Link href="/">homepage</Link>
         </p>
       </main>
--- templates/default/src/pages/index/index.jsx
+++ templates/default/src/pages/index/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
-import GlobalHead from '../../components/organisms/GlobalHead'
+import PageTitle from '../../components/organisms/PageTitle'
 
 const Home = () => {
   return (
     <>
-      <GlobalHead title="%%APPNAME%%" />
+      <PageTitle title="%%APPNAME%%" />
       <p>Welcome to the boilerplate React app using NextJS!</p>
       <p>
         Try navigating to
-
         <Link href="/example/second-page">another page</Link>
-
         and observe the URL changing
       </p>
     </>
   )
+}
+
+// estlin-disable-next-line
+Home.getInitialProps = async context => {
+  /* Here you can make your pre-fetch for this page, it's a pre-rendering step
+   * only available for pages.
+   * Useful parameters in context object: req, res, query (passed from 4th render argument)
+   * It expects an object with props to be returned
+   */
+  return {}
 }
 
 export default Home
