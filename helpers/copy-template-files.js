3e1250a8b4a7f99e503600a84a369795d845684e
Anthony McLin <git@anthonymclin.com>
2021-03-11 18:08:54 -0800

    feat(deps): adopt Husky v5 hook syntax
    Husky v5 uses a different syntax for triggering commit and push hooks.
    
    Closes #646
    
    Fixes #631

--- helpers/copy-template-files.js
+++ helpers/copy-template-files.js
@@ -1,24 +1,25 @@
 const cpy = require('cpy')
 const path = require('path')
 
-const copyTemplateFiles = (root, dir) => cpy(['**', '.dependabot/**'], root, {
+const copyTemplateFiles = (root, dir) =>
+  cpy(['**', '.dependabot/**', '.husky/**'], root, {
     parents: true,
     cwd: path.join(__dirname, '..', 'templates', dir),
-    rename: name => {
+    rename: (name) => {
       const dotFiles = [
         'babelrc',
         'editorconfig',
         'eslintrc.json',
         'gitignore',
-        'travis.yml'
+        'travis.yml',
       ]
       if (dotFiles.indexOf(name) > -1) {
         return '.'.concat(name)
       }
       return name
-    }
+    },
   })
 
 module.exports = {
-  copyTemplateFiles
+  copyTemplateFiles,
 }
